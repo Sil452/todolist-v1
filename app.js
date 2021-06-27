bac861a52b528c5a7e234682f379dbb950a9deca
silvia pintus <silviapintus.93@gmail.com>
2021-06-27 14:36:53 +0100

    getting current day + testing ejs

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-  res.send("<h1>Hello</h1>");
+
+//res.render uses the view engine that I set up previously to render a particular page in this case "list.ejs"
+app.get('/', (req, res) => {
+
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"];
+  const dayIndex = new Date().getDay();
+  const currentDay = weekday[dayIndex];
+
+  res.render('list', {currentDay: currentDay});
 });
 
 
