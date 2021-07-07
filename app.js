713855a8669e25dd5f68712db8d51a2e15746cde
silvia pintus <silviapintus.93@gmail.com>
2021-07-07 13:11:20 +0100

    add new task

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
+let tasks = ["Wake up!"];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
-//res.render uses the view engine that I set up previously to render a particular page in this case "list.ejs"
 app.get('/', (req, res) => {
 
   const today = new Date();
@@ -19,11 +20,21 @@ app.get('/', (req, res) => {
   };
 
   const currentDay = today.toLocaleDateString("en-US", options);
-
-  res.render('list', {currentDay: currentDay});
+  
+  //res.render uses the view engine that I set up previously to render a particular page in this case "list.ejs"
+  res.render('list', {currentDay: currentDay, tasks: tasks});
 });
 
+// when a post request is triggered we will save the value of newTask to the const newTask. Then it will redirect us to the home route,
+// where it will trigger the app.get for home route that will render list.ejs and parse currentDay and newTask.
 
+app.post("/", function(req, res){
+
+  const newTask = req.body.newTask;
+  tasks.push(newTask);
+
+  res.redirect("/");
+});
 
 
 
